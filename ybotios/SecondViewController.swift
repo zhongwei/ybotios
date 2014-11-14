a42341c94850ce0e58408d855a94b4f3d77f1c44
Wei Zhong <zhongwei99@163.com>
2014-11-14 20:37:10 +0800

    Initial Commit

---
+++ ybotios/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ybotios
+//
+//  Created by Zhong Wei on 14/11/14.
+//  Copyright (c) 2014年 ybot. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
